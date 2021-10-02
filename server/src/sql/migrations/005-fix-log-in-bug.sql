fbaedc35c5ec783662a00f84a362aa47d7de1e76
Gareth Latty <gareth@lattyware.co.uk>
2021-10-02 22:18:51 +0100

    Fix bug with log-in when a user has no active bets.

---
+++ server/src/sql/migrations/005-fix-log-in-bug.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE VIEW jasb.users_with_stakes AS
+  SELECT
+    users.*,
+    COALESCE(SUM(stakes.amount)::INT, 0) AS staked
+  FROM users
+    LEFT JOIN jasb.stakes ON users.id = stakes.owner
+    LEFT JOIN jasb.bets ON stakes.game = bets.game AND stakes.bet = bets.id
+  WHERE is_active(bets.progress)
+  GROUP BY users.id;
